86fe16cb355561af5443fc6e69d1b399fa1d90e7
Tiffany Lam <tlam5@sbcglobal.net>
2017-06-01 11:51:10 -0700

    Include prop-types module

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
+var App = require('./components/App');
 require('./index.css');
 
 class App extends React.Component {
